f2c9a968323fb473532f466940eb9ea2c65850b1
David Connet <dcon@agilityrecordbook.com>
2003-10-21 17:51:32 +0000

    Fixed copyright notice ("(c)" to CopyrightSymbol)

--- src/stdafx.h
+++ src/stdafx.h
@@ -1,19 +1,18 @@
 #pragma once
 
 /*
- * Copyright (c) 2002-2003 David Connet.
- * All Rights Reserved.
+ * Copyright © 2002-2003 David Connet. All Rights Reserved.
  *
  * Permission to use, copy, modify and distribute this software and its
  * documentation for any purpose and without fee is hereby granted, provided
  * that the above copyright notice appear in all copies, that both the
  * copyright notice and this permission notice appear in supporting
- * documentation, and that the names of David Connet, AgilityBook,
- * AgilityRecordBook or "Agility Record Book" not be used in advertising or
- * publicity pertaining to distribution of the software without specific,
- * written prior permission. David Connet makes no representations about
- * the suitability of this software for any purpose. It is provided "as is"
- * without express or implied warranty.
+ * documentation, and that the names of David Connet, dcon Software,
+ * AgilityBook, AgilityRecordBook or "Agility Record Book" not be used in
+ * advertising or publicity pertaining to distribution of the software
+ * without specific, written prior permission. David Connet makes no
+ * representations about the suitability of this software for any purpose.
+ * It is provided "as is" without express or implied warranty.
  *
  * DAVID CONNET DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
  * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO
@@ -23,8 +22,8 @@
  * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  * PERFORMANCE OF THIS SOFTWARE.
  *
- * OSI Certified Open Source Software
  * http://opensource.org
+ * Open Source Historical Permission Notice and Disclaimer.
  */
 
 /**
