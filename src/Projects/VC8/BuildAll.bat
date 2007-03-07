12d31bc630b600e66374c09928fc7e3bf74db409
David Connet <dcon@agilityrecordbook.com>
2007-03-07 21:58:31 +0000

    Switched to WiX for installations

--- src/Projects/VC8/BuildAll.bat
+++ src/Projects/VC8/BuildAll.bat
@@ -1,10 +1,9 @@
-REM Note, the setup projects do not get cleaned. Doesn't appear possible...
 devenv AgilityBook.sln /clean "Unicode Release|Win32"
 devenv AgilityBook.sln /clean "Release|Win32"
 devenv AgilityBook.sln /clean "Unicode Release|x64"
 
-devenv AgilityBook.sln /build "Unicode Release|Win32" /project AgilityBookSetup.vdproj /out bldWin.txt
+devenv AgilityBook.sln /build "Unicode Release|Win32" /out bldWin.txt
+devenv AgilityBook.sln /build "Release|Win32" /out bldWin98.txt
+devenv AgilityBook.sln /build "Unicode Release|x64" /out bldWin64.txt
 
-devenv AgilityBook.sln /build "Release|Win32" /project AgilityBookSetup98.vdproj /out bldWin98.txt
-
-devenv AgilityBook.sln /build "Unicode Release|x64" /project AgilityBookSetup64.vdproj /out bldWin64.txt
+GenMSI.py
