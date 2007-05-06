12647cbc93fe715c57173c3c4ae3a70dc4e122e1
David Connet <dcon@agilityrecordbook.com>
2007-05-06 07:12:19 +0000

    Switch back to MS vdproj setup files

--- src/Projects/VC8/BuildAll.bat
+++ src/Projects/VC8/BuildAll.bat
@@ -2,8 +2,10 @@ devenv AgilityBook.sln /clean "Unicode Release|Win32"
 devenv AgilityBook.sln /clean "Release|Win32"
 devenv AgilityBook.sln /clean "Unicode Release|x64"
 
-devenv AgilityBook.sln /build "Unicode Release|Win32" /out bldWin.txt
-devenv AgilityBook.sln /build "Release|Win32" /out bldWin98.txt
-devenv AgilityBook.sln /build "Unicode Release|x64" /out bldWin64.txt
+devenv AgilityBook.sln /out bldWin32.txt /build "Unicode Release|Win32"
+devenv AgilityBook.sln /out bldWin98.txt /build "Release|Win32"
+devenv AgilityBook.sln /out bldWin64.txt /build "Unicode Release|x64"
 
-GenMSI.py
+REM Todo: Fix WiX, then trash vdproj
+REM GenMSI.py
+devenv AgilityBookSetup.sln /out bldWinsetup.txt /build
