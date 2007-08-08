50f18b9d261a11a47d3a2a86e5f0bc009e67665d
David Connet <dcon@agilityrecordbook.com>
2007-08-08 16:49:38 +0000

    Move arbzlib from DLL to internal

--- bin/CopyDLLs.bat
+++ bin/CopyDLLs.bat
@@ -1,27 +1,17 @@
 xcopy /y ..\lib\VC6\xerces-c_2_7.dll VC6\Release\
-xcopy /y ..\lib\VC6\arbzlib.dll VC6\Release\
 xcopy /y ..\lib\VC6\xerces-c_2_7D.dll VC6\Debug\
-xcopy /y ..\lib\VC6\arbzlib.dll VC6\Debug\
 
 xcopy /y ..\lib\VC7\xerces-c_2_7.dll VC7\Release\
-xcopy /y ..\lib\VC7\arbzlib.dll VC7\Release\
 xcopy /y ..\lib\VC7\xerces-c_2_7D.dll VC7\Debug\
 xcopy /y ..\lib\VC7\xerces-c_2_7D.pdb VC7\Debug\
-xcopy /y ..\lib\VC7\arbzlib.dll VC7\Debug\
 
 xcopy /y ..\lib\VC8\xerces-c_2_7.dll VC8\Release\
-xcopy /y ..\lib\VC8\arbzlib.dll VC8\Release\
 xcopy /y ..\lib\VC8\xerces-c_2_7D.dll VC8\Debug\
 xcopy /y ..\lib\VC8\xerces-c_2_7D.pdb VC8\Debug\
-xcopy /y ..\lib\VC8\arbzlib.dll VC8\Debug\
 xcopy /y ..\lib\VC8\xerces-c_2_7.dll "VC8\Unicode Release\"
-xcopy /y ..\lib\VC8\arbzlib.dll "VC8\Unicode Release\"
 xcopy /y ..\lib\VC8\xerces-c_2_7D.dll "VC8\Unicode Debug\"
 xcopy /y ..\lib\VC8\xerces-c_2_7D.pdb "VC8\Unicode Debug\"
-xcopy /y ..\lib\VC8\arbzlib.dll "VC8\Unicode Debug\"
 
 xcopy /y ..\lib\VC8x64\xerces-c_2_7.dll "VC8x64\Unicode Release\"
-xcopy /y ..\lib\VC8x64\arbzlib.dll "VC8x64\Unicode Release\"
 xcopy /y ..\lib\VC8x64\xerces-c_2_7D.dll "VC8x64\Unicode Debug\"
 xcopy /y ..\lib\VC8x64\xerces-c_2_7D.pdb "VC8x64\Unicode Debug\"
-xcopy /y ..\lib\VC8x64\arbzlib.dll "VC8x64\Unicode Debug\"
