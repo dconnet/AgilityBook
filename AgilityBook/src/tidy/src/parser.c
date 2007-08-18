6e8ebb19951d2aeb10c2e25edf3e32df5b736c64
David Connet <dcon@agilityrecordbook.com>
2007-08-18 15:09:27 +0000

    Update tidy with new SourceForge release

--- AgilityBook/src/tidy/src/parser.c
+++ AgilityBook/src/tidy/src/parser.c
@@ -6,8 +6,8 @@
   CVS Info :
 
     $Author: arnaud02 $ 
-    $Date: 2007/05/30 16:47:31 $ 
-    $Revision: 1.184 $ 
+    $Date: 2007/08/15 17:43:02 $ 
+    $Revision: 1.186 $ 
 
 */
 
@@ -687,7 +687,7 @@ static Bool InsertMisc(Node *element, Node *node)
         Node* root = element;
         while ( root && root->parent )
             root = root->parent;
-        if ( root )
+        if ( root && !(root->content && root->content->type == XmlDecl))
         {
           TY_(InsertNodeAtStart)( root, node );
           return yes;
@@ -1999,8 +1999,8 @@ static Bool FindLastLI( Node *list, Node **lastli )
     Node *node;
 
     *lastli = NULL;
-    for ( node = list->content; node && node->next; node = node->next )
-        if ( nodeIsLI(node) && node && node->type == StartTag )
+    for ( node = list->content; node ; node = node->next )
+        if ( nodeIsLI(node) && node->type == StartTag )
             *lastli=node;
     return *lastli ? yes:no;
 }
@@ -2101,7 +2101,10 @@ void TY_(ParseList)(TidyDocImpl* doc, Node *list, GetTokenMode ARG_UNUSED(mode))
                rendering of most browsers. */    
             if ( nodeIsOL(list) && FindLastLI(list, &lastli) )
             {
+                /* Create a node for error reporting */
+                node = TY_(InferredTag)(doc, TidyTag_LI);
                 TY_(ReportError)(doc, list, node, MISSING_STARTTAG );
+                TY_(FreeNode)( doc, node);
                 node = lastli;
             }
             else
