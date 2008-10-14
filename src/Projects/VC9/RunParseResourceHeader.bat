b21d5c2774779a89e97b4cec91e80aa54c75558f
David Connet <dcon@agilityrecordbook.com>
2008-10-14 23:37:01 +0000

    Add more x64 support

---
+++ src/Projects/VC9/RunParseResourceHeader.bat
@@ -0,0 +1,39 @@
+@echo off
+REM This script is called in the post-build step to run the unittest program
+if ("%1")==("") goto usage
+if ("%2")==("") goto usage
+
+if ("%2")==("Win32") goto w32
+if ("%2")==("x64") goto x64
+
+echo Error: Unknown target: %2
+goto done
+
+:w32
+REM 32bit on 32bit
+if ("%PROCESSOR_ARCHITECTURE%")==("x86") goto run
+goto nope
+
+:x64
+REM 64bit on 64bit
+if ("%PROCESSOR_ARCHITECTURE%")==("AMD64") goto run
+REM 64bit on Wow64 (32bit cmd shell spawned from msdev)
+if ("%PROCESSOR_ARCHITEW6432%")==("AMD64") goto run
+goto nope
+
+:nope
+echo WARNING: Unable to run %2 binary on %PROCESSOR_ARCHITECTURE%
+goto done
+
+:run
+cd $(ProjectDir)..\..
+BuildHelp.py "%1" VC9%2
+
+goto done
+
+:usage
+echo Usage: %0 Configuration PlatformName
+echo PlatformName = Win32 x64
+echo Ex (in devenv postbuild): %0 "$(ConfigurationName)" $(PlatformName)
+
+:done
