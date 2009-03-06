8b8515e6b0e5c361e779ff43ab653c2fe93f3606
David Connet <dcon@agilityrecordbook.com>
2009-03-06 02:55:40 +0000

    Fixed a problem in 'compile.py', fixed platform configurations in vc9

---
+++ src/Projects/CompileWX.bat
@@ -0,0 +1,90 @@
+@echo off
+rem This is my quick-and-easy way to compile wxWidgets on Windows.
+rem It assumes wxWidgets is in "\wx" and ARB is in "\AgilityBook\src\AgilityBook"
+
+set _DO_UNICODE=0
+set _DO_MBCS=0
+
+if ("%1")==("vc6") goto vc6
+if ("%1")==("vc7") goto vc7
+if ("%1")==("vc8") goto vc8
+if ("%1")==("vc9") goto vc9
+if ("%1")==("vc9x64") goto vc9x64
+goto usage
+
+:vc6
+title VC6
+call "C:\Program Files\Microsoft Visual Studio\VC98\bin\vcvars32.bat"
+echo on
+set _DO_MBCS=1
+set _CFG=_VC6.0
+set _RUNTIME_LIBS=dynamic
+REM vc_lib_VC6.0
+goto doit
+
+:vc7
+title VC7
+call "C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\Tools\vsvars32.bat"
+set _DO_MBCS=1
+set _CFG=_VC7.1s
+set _RUNTIME_LIBS=static
+REM vc_lib_VC7.1s
+goto doit
+
+:vc8
+title VC8
+call "C:\Program Files\Microsoft Visual Studio 8.0\VC\vcvarsall.bat" x86
+set _DO_UNICODE=1
+set _DO_MBCS=1
+set _CFG=_VC8.0s
+set _RUNTIME_LIBS=static
+REM vc_lib_VC8.0s
+goto doit
+
+:vc9
+title VC9
+call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
+set _DO_UNICODE=1
+set _DO_MBCS=1
+set _CFG=_VC9.0s
+set _RUNTIME_LIBS=static
+REM vc_lib_VC9.0s
+goto doit
+
+:vc9x64
+title VC9x64
+call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86_amd64
+set _DO_UNICODE=1
+set _TARGET_CPU=TARGET_CPU=amd64
+set _CFG=_VC9.0s
+set _RUNTIME_LIBS=static
+REM vc_amd64_lib_VC9.0s
+goto doit
+
+
+:doit
+cd \wx\build\msw
+
+if ("%_DO_UNICODE%")==("0") goto next
+nmake -f makefile.vc BUILD=release            UNICODE=1 RUNTIME_LIBS=%_RUNTIME_LIBS% %_TARGET_CPU% CFG=%_CFG%
+nmake -f makefile.vc BUILD=debug DEBUG_INFO=1 UNICODE=1 RUNTIME_LIBS=%_RUNTIME_LIBS% %_TARGET_CPU% CFG=%_CFG%
+
+:next
+if ("%_DO_MBCS%")==("0") goto next2
+nmake -f makefile.vc BUILD=release            RUNTIME_LIBS=%_RUNTIME_LIBS% CFG=%_CFG%
+nmake -f makefile.vc BUILD=debug DEBUG_INFO=1 RUNTIME_LIBS=%_RUNTIME_LIBS% CFG=%_CFG%
+
+:next2
+cd \AgilityBook\src\AgilityBook\src\Projects
+goto done
+
+:usage
+echo Usage: %0 vc6 or vc7 or vc8 or vc9 or vc9x64
+
+:done
+set _DO_UNICODE=
+set _DO_MBCS=
+set _TARGET_CPU=
+set _CFG=
+set _RUNTIME_LIBS=
+
--- src/Win/res/compile.py
+++ src/Win/res/compile.py
@@ -3,6 +3,7 @@
 # Requires gettext (msgcat/msgfmt) and winzip command line addon (wzzip)
 #
 # Revision History
+# 2009-03-05 DRC Fixed some parameter passing issues (spaces/hyphens in names)
 # 2009-03-01 DRC Support multiple po files (by using msgcat)
 # 2009-01-02 DRC Updated to support creation of data files
 """compile.py [-d] executableDir targetname
@@ -16,6 +17,7 @@ import glob
 import os
 import shutil
 import sys
+from subprocess import *
 
 # By default, gettext is in the path, winzip is not.
 # Command line addon to winzip
@@ -33,12 +35,18 @@ def ReadPipe(logFile, cmd):
 		else:
 			break
 
-def RunCommand(command):
+
+# Some commands generate messages on stderr that are interesting.
+# Some are just plain annoying (wzzip)
+def RunCommand(command, toastErr):
 	print 'Running:', command
-	(childin, childout) = os.popen4(command)
-	childin.close()
-	ReadPipe(sys.stdout, childout)
-	childout.close()
+	if toastErr:
+		# Map stderr to a pipe that we ignore
+		p = Popen(command, stderr=PIPE, stdout=PIPE)
+	else:
+		# Map stderr to stdout
+		p = Popen(command, stderr=STDOUT, stdout=PIPE)
+	ReadPipe(sys.stdout, p.stdout)
 
 
 def main():
@@ -69,23 +77,26 @@ def main():
 	for file in ('en', 'fr'):
 		autogen = file + '\\' + autogenFile
 		# -t: output encoding
-		cmd = 'msgcat -t "utf-8" -o ' + autogen + ' ' + file + r'\arb.po'
+		cmd = ['msgcat.exe', '-t', 'utf-8', '-o', autogen, file + r'\arb.po']
 		for po in glob.glob(file + r'\*.po'):
 			if po != file + r'\arb.po' and po != autogen:
-				cmd += ' ' + po
-		RunCommand(cmd)
+				cmd += [po]
+		RunCommand(cmd, 0)
 		installPath = langDir + '\\' + file
 		if not os.access(installPath, os.F_OK):
 			os.mkdir(installPath)
 		# -v: verbose
 		# -c: perform all checks (format,header,domain)
 		# -f: Use fuzzy entres in output
-		cmd = 'msgfmt -v -c -f --strict -o "' + installPath + r'\arb.mo" ' + autogen
-		RunCommand(cmd)
-		if not bDebug:
+		cmd = ['msgfmt.exe', '-v', '-c', '-f', '--strict', '-o', installPath + r'\arb.mo', autogen]
+		# msgfmt generates interesting messages to stderr, don't toast them.
+		RunCommand(cmd, 0)
+		if not bDebug and os.access(autogen, os.F_OK):
 			os.remove(autogen)
 
-	RunCommand('"' + winzip + '" -a ' + executableDir + '\\' + targetname + '.dat DefaultConfig.xml AgilityRecordBook.dtd')
+	# Winzip generates "..." and backspaces while searching for files (stderr)
+	# Toast em!
+	RunCommand([winzip, '-a', executableDir + '\\' + targetname + '.dat', 'DefaultConfig.xml', 'AgilityRecordBook.dtd'], 1)
 
 
 main()
