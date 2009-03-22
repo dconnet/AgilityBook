74a9b1e97814d72345e5792cebf40cbeada333e3
Steve Schwarz <steve@agilitynerd.com>
2009-03-22 19:34:44 +0000

    made compile.py more crossplatform

--- AgilityBook/src/Win/res/compile.py
+++ AgilityBook/src/Win/res/compile.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Generate the message libraries and data files
 #
 # Requires gettext (msgcat/msgfmt) and winzip command line addon (wzzip)
@@ -14,89 +15,89 @@ targetname: Name of .mo file to generate
 
 import getopt
 import glob
-import os
-import shutil
+import os, os.path
 import sys
-from subprocess import *
+import subprocess
+import zipfile
 
 # By default, gettext is in the path, winzip is not.
 # Command line addon to winzip
-winzip = 'c:\Program Files\WinZip\WZZIP'
+# winzip = 'c:\Program Files\WinZip\WZZIP'
 
 # This file is generated with msgcat of all po files (arb.po is first)
 autogenFile = 'autogen.po'
 
 
 def ReadPipe(logFile, cmd):
-	while (1):
-		line = cmd.readline()
-		if line:
-			print >>logFile, line,
-		else:
-			break
+    while (1):
+        line = cmd.readline()
+        if line:
+            print >>logFile, line,
+        else:
+            break
 
 
 # Some commands generate messages on stderr that are interesting.
 # Some are just plain annoying (wzzip)
 def RunCommand(command, toastErr):
-	print 'Running:', command
-	if toastErr:
-		# Map stderr to a pipe that we ignore
-		p = Popen(command, stderr=PIPE, stdout=PIPE)
-	else:
-		# Map stderr to stdout
-		p = Popen(command, stderr=STDOUT, stdout=PIPE)
-	ReadPipe(sys.stdout, p.stdout)
+    print 'Running:', command
+    if toastErr:
+        # Map stderr to a pipe that we ignore
+        p = subprocess.Popen(command, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    else:
+        # Map stderr to stdout
+        p = subprocess.Popen(command, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+    ReadPipe(sys.stdout, p.stdout)
 
 
 def main():
-	bDebug = 0
-	executableDir = ''
-	try:
-		opts, args = getopt.getopt(sys.argv[1:], 'd')
-	except getopt.error, msg:
-		print msg
-		print 'Usage:', __doc__
-		return
-	for o, a in opts:
-		if '-d' == o:
-			bDebug = 1
-	if 2 != len(args):
-		print 'Usage:', __doc__
-		return
-	executableDir = args[0]
-	targetname = args[1]
+    bDebug = 0
+    executableDir = ''
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'd')
+    except getopt.error, msg:
+        print msg
+        print 'Usage:', __doc__
+        return
+    for o, a in opts:
+        if '-d' == o:
+            bDebug = 1
+    if 2 != len(args):
+        print 'Usage:', __doc__
+        return
+    executableDir = args[0]
+    targetname = args[1]
 
-	if not os.access(executableDir, os.F_OK):
-		print executableDir, 'does not exist'
+    if not os.access(executableDir, os.F_OK):
+        print executableDir, 'does not exist'
 
-	langDir = executableDir + r'\lang'
-	if not os.access(langDir, os.F_OK):
-		os.mkdir(langDir)
+    langDir = os.path.join(executableDir, r'lang')
+    if not os.access(langDir, os.F_OK):
+        os.mkdir(langDir)
 
-	for file in ('en', 'fr'):
-		autogen = file + '\\' + autogenFile
-		# -t: output encoding
-		cmd = ['msgcat', '-t', 'utf-8', '-o', autogen, file + r'\arb.po']
-		for po in glob.glob(file + r'\*.po'):
-			if po != file + r'\arb.po' and po != autogen:
-				cmd += [po]
-		RunCommand(cmd, 0)
-		installPath = langDir + '\\' + file
-		if not os.access(installPath, os.F_OK):
-			os.mkdir(installPath)
-		# -v: verbose
-		# -c: perform all checks (format,header,domain)
-		# -f: Use fuzzy entres in output
-		cmd = ['msgfmt', '-v', '-c', '-f', '--strict', '-o', installPath + r'\arb.mo', autogen]
-		# msgfmt generates interesting messages to stderr, don't toast them.
-		RunCommand(cmd, 0)
-		if not bDebug and os.access(autogen, os.F_OK):
-			os.remove(autogen)
+    for file in ('en', 'fr'):
+        autogen = os.path.join(file, autogenFile)
+        # -t: output encoding
+        cmd = ['msgcat', '-t', 'utf-8', '-o', autogen, os.path.join(file, r'arb.po')]
+        for po in glob.glob(os.path.join(file, r'*.po')):
+            if po != os.path.join(file, r'arb.po') and po != autogen:
+                cmd += [po]
+        RunCommand(cmd, 0)
+        installPath = os.path.join(langDir, file)
+        if not os.access(installPath, os.F_OK):
+            os.mkdir(installPath)
+        # -v: verbose
+        # -c: perform all checks (format,header,domain)
+        # -f: Use fuzzy entres in output
+        cmd = ['msgfmt', '-v', '-c', '-f', '--strict', '-o', os.path.join(installPath, r'arb.mo'), autogen]
+        # msgfmt generates interesting messages to stderr, don't toast them.
+        RunCommand(cmd, 0)
+        if not bDebug and os.access(autogen, os.F_OK):
+            os.remove(autogen)
 
-	# Winzip generates "..." and backspaces while searching for files (stderr)
-	# Toast em! (that's the '1' that's the 2nd param of RunCommand)
-	RunCommand([winzip, '-a', executableDir + '\\' + targetname + '.dat', 'DefaultConfig.xml', 'AgilityRecordBook.dtd'], 1)
-
+    zip = zipfile.ZipFile(os.path.join(executableDir, targetname + '.dat'), 'w')
+    zip.write('DefaultConfig.xml')
+    zip.write('AgilityRecordBook.dtd')
+    zip.close()
 
 main()
