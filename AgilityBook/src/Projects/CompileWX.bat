8d1d63f32ecb047b9836fd2a1a70f852bb5669cd
David Connet <dcon@agilityrecordbook.com>
2009-04-25 21:20:55 +0000

    Tweaked wxwidget build script to be easier to use on windows

--- AgilityBook/src/Projects/CompileWX.bat
+++ AgilityBook/src/Projects/CompileWX.bat
@@ -1,21 +1,30 @@
 @echo off
 rem This is my quick-and-easy way to compile wxWidgets on Windows.
-rem It assumes ARB is in "\AgilityBook\src\AgilityBook"
 
-set _DO_UNICODE=0
-set _DO_MBCS=0
+set _PROGNAME=%0
 
-set _TRY_MBCS=0
-set _TRY_UNICODE=0
-if ("%2")==("mbcs") set _TRY_MBCS=1
-if ("%3")==("mbcs") set _TRY_MBCS=1
-if ("%2")==("unicode") set _TRY_UNICODE=1
-if ("%3")==("unicode") set _TRY_UNICODE=1
-if ("%_TRY_MBCS%")==("1") or ("%_TRY_UNICODE%")==("1") goto check1
-set _TRY_MBCS=1
-set _TRY_UNICODE=1
+if not ("%1")==("all") goto :args
+call :args dynamic mbcs vc6
+call :args static mbcs vc7
+call :args static mbcs vc8
+call :args static unicode vc9
+call :args static unicode vc9x64
+goto done
 
-:check1
+:args
+set _DO_SHIFT=0
+set _RUNTIME_LIBS=static
+if ("%1")==("static") set _RUNTIME_LIBS=static&& set _DO_SHIFT=1
+if ("%1")==("dynamic") set _RUNTIME_LIBS=dynamic&& set _DO_SHIFT=1
+if ("%_DO_SHIFT%")==("1") shift && set _DO_SHIFT=0
+
+set _DO_UNICODE=1
+if ("%1")==("mbcs") set _DO_UNICODE=0&& set _DO_SHIFT=1
+if ("%1")==("unicode") set _DO_UNICODE=1&& set _DO_SHIFT=1
+if ("%_DO_SHIFT%")==("1") shift && set _DO_SHIFT=0
+
+if ("%_RUNTIME_LIBS%")==("static") set _CFGEND=s
+
 if ("%1")==("vc6") goto vc6
 if ("%1")==("vc7") goto vc7
 if ("%1")==("vc8") goto vc8
@@ -24,83 +33,65 @@ if ("%1")==("vc9x64") goto vc9x64
 goto usage
 
 :vc6
+if ("%_DO_UNICODE%")==("1") echo Error: VC6 doesn't do unicode && goto usage
+if ("%_RUNTIME_LIBS%")==("static") echo Error: VC6 doesn't do static && goto usage
 title VC6
 call "C:\Program Files\Microsoft Visual Studio\VC98\bin\vcvars32.bat"
-echo on
-if ("%_TRY_MBCS%")==("1") set _DO_MBCS=1
 set _CFG=_VC6.0
-set _RUNTIME_LIBS=dynamic
 set _CPPFLAGS=
-REM vc_lib_VC6.0
-goto doit
+goto :doit
 
 :vc7
 title VC7
 call "C:\Program Files\Microsoft Visual Studio .NET 2003\Common7\Tools\vsvars32.bat"
-if ("%_TRY_MBCS%")==("1") set _DO_MBCS=1
-set _CFG=_VC7.1s
-set _RUNTIME_LIBS=static
+if ("%_DO_UNICODE%")==("1") echo Error: VC7 doesn't do unicode && goto usage
+set _CFG=_VC7.1
 set _CPPFLAGS=
-REM vc_lib_VC7.1s
-goto doit
+goto :doit
 
 :vc8
 title VC8
-call "C:\Program Files\Microsoft Visual Studio 8.0\VC\vcvarsall.bat" x86
-if ("%_TRY_UNICODE%")==("1") set _DO_UNICODE=1
-if ("%_TRY_MBCS%")==("1") set _DO_MBCS=1
-set _CFG=_VC8.0s
-set _RUNTIME_LIBS=static
+call "C:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat" x86
+set _CFG=_VC8.0
 set _CPPFLAGS=
-REM vc_lib_VC8.0s
-goto doit
+goto :doit
 
 :vc9
 title VC9
 call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
-if ("%_TRY_UNICODE%")==("1") set _DO_UNICODE=1
-if ("%_TRY_MBCS%")==("1") set _DO_MBCS=1
-set _CFG=_VC9.0s
-set _RUNTIME_LIBS=static
+set _CFG=_VC9.0
 set _CPPFLAGS=/D_SECURE_SCL=1 /D_SECURE_SCL_THROWS=1
-REM vc_lib_VC9.0s
-goto doit
+goto :doit
 
 :vc9x64
+if ("%_DO_UNICODE%")==("0") echo Error: VC9x64 doesn't do mbcs && goto usage
 title VC9x64
 call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86_amd64
-if ("%_TRY_UNICODE%")==("1") set _DO_UNICODE=1
 set _TARGET_CPU=TARGET_CPU=amd64
-set _CFG=_VC9.0s
-set _RUNTIME_LIBS=static
+set _CFG=_VC9.0
 set _CPPFLAGS=/D_SECURE_SCL=1 /D_SECURE_SCL_THROWS=1
-REM vc_amd64_lib_VC9.0s
-goto doit
+goto :doit
 
 
 :doit
 cd %WXWIN%\build\msw
 
-if ("%_DO_UNICODE%")==("0") goto next
-nmake -f makefile.vc BUILD=release            UNICODE=1 RUNTIME_LIBS=%_RUNTIME_LIBS% %_TARGET_CPU% CFG=%_CFG% CPPFLAGS="%_CPPFLAGS%"
-nmake -f makefile.vc BUILD=debug DEBUG_INFO=1 UNICODE=1 RUNTIME_LIBS=%_RUNTIME_LIBS% %_TARGET_CPU% CFG=%_CFG% CPPFLAGS="%_CPPFLAGS%"
+echo nmake -f makefile.vc BUILD=release            UNICODE=%_DO_UNICODE% RUNTIME_LIBS=%_RUNTIME_LIBS% %_TARGET_CPU% CFG=%_CFG%%_CFGEND% CPPFLAGS="%_CPPFLAGS%"
+echo nmake -f makefile.vc BUILD=debug DEBUG_INFO=1 UNICODE=%_DO_UNICODE% RUNTIME_LIBS=%_RUNTIME_LIBS% %_TARGET_CPU% CFG=%_CFG%%_CFGEND% CPPFLAGS="%_CPPFLAGS%"
 
-:next
-if ("%_DO_MBCS%")==("0") goto next2
-nmake -f makefile.vc BUILD=release            RUNTIME_LIBS=%_RUNTIME_LIBS% CFG=%_CFG% CPPFLAGS="%_CPPFLAGS%"
-nmake -f makefile.vc BUILD=debug DEBUG_INFO=1 RUNTIME_LIBS=%_RUNTIME_LIBS% CFG=%_CFG% CPPFLAGS="%_CPPFLAGS%"
-
-:next2
 cd \AgilityBook\src\AgilityBook\src\Projects
 goto done
 
 :usage
-echo Usage: %0 vc6 or vc7 or vc8 or vc9 or vc9x64 [mbcs] [unicode]
+echo Usage: %_PROGNAME% all
+echo Usage: %_PROGNAME% [dynamic/static] [mbcs/unicode] vc6/vc7/vc8/vc9/vc9x64
 
 :done
-set _DO_UNICODE=
-set _DO_MBCS=
-set _TARGET_CPU=
 set _CFG=
+set _CFGEND=
+set _CPPFLAGS=
+set _DO_SHIFT=
+set _DO_UNICODE=
+set _PROGNAME=
 set _RUNTIME_LIBS=
-
+set _TARGET_CPU=
