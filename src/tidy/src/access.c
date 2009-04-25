ffb57959d213b21312dd4298b78a89fee2175aff
David Connet <dcon@agilityrecordbook.com>
2009-04-25 15:55:30 +0000

    update libtidy to current sourceforge sources (bumped build number)

--- src/tidy/src/access.c
+++ src/tidy/src/access.c
@@ -1,14 +1,14 @@
 /* access.c -- carry out accessibility checks
 
   Copyright University of Toronto
-  Portions (c) 1998-2008 (W3C) MIT, ERCIM, Keio University
+  Portions (c) 1998-2009 (W3C) MIT, ERCIM, Keio University
   See tidy.h for the copyright notice.
   
   CVS Info :
 
     $Author: arnaud02 $ 
-    $Date: 2008/08/11 10:08:23 $ 
-    $Revision: 1.40 $ 
+    $Date: 2009/03/25 22:04:35 $ 
+    $Revision: 1.42 $ 
 
 */
 
@@ -1580,21 +1580,6 @@ static void CheckMultiHeaders( TidyDocImpl* doc, Node* node )
                     {
                         temp = TNode->content;
 
-                        if ( nodeIsTH(temp) )
-                        {
-                            AttVal* av;
-                            for (av = temp->attributes; av != NULL; av = av->next)
-                            {
-                                if ( attrIsROWSPAN(av) )
-                                {
-                                    if (atoi(av->value) > 1)
-                                    {
-                                        validColSpanRows = no;
-                                    }
-                                }
-                            }
-                        }
-
                         /* The number of TH elements found within TR element */
                         if (flag == 0)
                         {
@@ -1609,13 +1594,13 @@ static void CheckMultiHeaders( TidyDocImpl* doc, Node* node )
                                     AttVal* av;
                                     for (av = temp->attributes; av != NULL; av = av->next)
                                     {
-                                        if ( attrIsCOLSPAN(av) )
-                                        {
-                                            if (atoi(av->value) > 1)
-                                            {
-                                                validColSpanColumns = no;
-                                            }
-                                        }
+                                        if ( attrIsCOLSPAN(av)
+                                             && (atoi(av->value) > 1) )
+                                            validColSpanColumns = no;
+
+                                        if ( attrIsROWSPAN(av)
+                                             && (atoi(av->value) > 1) )
+                                            validColSpanRows = no;
                                     }
                                 }
 
@@ -2772,7 +2757,7 @@ static Bool CheckMetaData( TidyDocImpl* doc, Node* node, Bool HasMetaData )
         }
             
         if ( !HasMetaData &&
-             nodeIsTITLE(node) &&
+             !nodeIsTITLE(node) &&
              TY_(nodeIsText)(node->content) )
         {
             ctmbstr word = textFromOneNode( doc, node->content );
