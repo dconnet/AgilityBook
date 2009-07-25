de2a908c135e7425aeabaf5a9b17c05a8812abd4
David Connet <dcon@agilityrecordbook.com>
2009-07-25 23:21:25 +0000

    Fix unix file conversion issues due to special (c) symbol

--- AgilityBook/src/Include/VersionNumber.h
+++ AgilityBook/src/Include/VersionNumber.h
@@ -74,5 +74,5 @@
 
 #define ARB_VERSION_CompanyName			"dcon Software"
 #define ARB_VERSION_FileDescription		"Agility Record Book"
-#define ARB_VERSION_LegalCopyright		"Copyright © 2002-2009 David Connet. All Rights Reserved."
+#define ARB_VERSION_LegalCopyright		"Copyright (c) 2002-2009 David Connet. All Rights Reserved."
 #define ARB_VERSION_ProductName			"Agility Record Book"
