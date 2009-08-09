74c67556bce37b8a3b3f5e03a12d7878ca554df5
David Connet <dcon@agilityrecordbook.com>
2009-08-09 17:28:10 +0000

    Fix build script (broken due to commented out 'cd' line)

--- src/Projects/Installer/BuildAll.bat
+++ src/Projects/Installer/BuildAll.bat
@@ -17,62 +17,56 @@ rd /q/s VC8Win32
 rd /q/s VC8x64
 rd /q/s VC9Win32
 rd /q/s VC9x64
+cd ..\src\Projects
 
 REM Targets:
 REM VC6
 REM  Configuration: 'Release'/'Debug' (non-unicode)
 REM  Platform: Win32
-REM  Targets: AgilityBook, cal_usdaa, LibTidy, LibXerces, LibZlib
+REM  Targets: AgilityBook, cal_usdaa, LibTidy
 REM VC7.1
 REM  Configuration: 'Release'/'Debug' (non-unicode)
 REM  Platform: Win32
-REM  Targets: AgilityBook, cal_usdaa, LibTidy, LibXerces, LibZlib
+REM  Targets: AgilityBook, cal_usdaa, LibTidy
 REM VC8
 REM  Configuration: 'Release'/'Debug'/'Unicode Release'/'Unicode Debug'
 REM  Platform: Win32, x64
 REM  ARBHelp: 'Release'/'Debug'
 REM  Targets:
 REM   AgilityBook: all
-REM   AgilityBookFRA: all
 REM   ARBHelp: no unicode
 REM   cal_usdaa: all
 REM   LibTidy: no unicode
-REM   LibXerces: no unicode
-REM   LibZlib: no unicode
-REM   ParseResourceHeader: all
 REM VC9
 REM  Configuration: 'Release'/'Debug'/'Release - No Unicode'/'Debug - No Unicode'
 REM  Platform: Win32, x64
 REM  Targets:
 REM   AgilityBook: unicode only
-REM   AgilityBookFRA: unicode only
 REM   ARBHelp: unicode only
 REM   cal_usdaa: unicode only
 REM   LibTidy: no unicode [debug/release=non unicode]
-REM   LibXerces: no unicode [debug/release=non unicode]
-REM   LibZlib: no unicode [debug/release=non unicode]
-REM   ParseResourceHeader: unicode only
 REM   TestARB: all
 
 REM We no longer support win98!!!
-REM cd ..\src\Projects\VC8
+REM cd VC8
 REM del bldWin98.txt
 REM Non-unicode, must do in VC8 since VC9 no longer supports win98
 REM (Release == non-unicode)
 REM call "c:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat" x86
 REM devenv AgilityBook.sln /out bldWin98.txt /build "Release|Win32"
+REM cd ..
 
-cd ..\VC9
+cd VC9
 del bldWin32.txt bldWin64.txt
 REM Unicode (Release == Unicode)
-
 call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86_amd64
 devenv AgilityBook.sln /out bldWin64.txt /build "Release|x64"
-
 call "C:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
 devenv AgilityBook.sln /out bldWin32.txt /build "Release|Win32"
+cd ..
 
-cd ..\Installer
+REM Go back to where we started
+cd Installer
 
 set PATH=%OLDPATH%
 set INCLUDE=%OLDINCLUDE%
