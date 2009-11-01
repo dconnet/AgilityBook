e7356cf9f90a7254861c949bd31cc2e81b289f67
David Connet <dcon@agilityrecordbook.com>
2009-11-01 17:13:36 +0000

    Add support to fall back to English parsing for decimal numbers. That allows someone in France to enter either "1,2" or "1.2"

--- src/ARB/ARBString.h
+++ src/ARB/ARBString.h
@@ -56,19 +56,46 @@ public:
 	static std::string tstringA(wxMemoryOutputStream const& inStr);
 	static std::string tstringA(wxString const& inStr);
 
-	// These functions mimic wx2.9.x. They enable wx2.8 usage.
-	// The To<type> uses locales.
+	// For consistency wit "ToC" routines
+	static bool ToLong(wxString const& inStr, long& outValue)
+	{
+		return inStr.ToLong(&outValue);
+	}
+	static long ToLong(wxString const& inStr)
+	{
+		long val = 0;
+		ToLong(inStr, val);
+		return val;
+	}
+	/// @note This function differs from the others - instead of only
+	/// parsing in the current locale, this will also fall back to English
+	/// ('.' for decimal)
+	static bool ToDouble(wxString const& inStr, double& outValue);
+	static double ToDouble(wxString const& inStr)
+	{
+		double val = 0.0;
+		ToDouble(inStr, val);
+		return val;
+	}
+
+	// These functions mimic wx2.9.x. They allow wx2.8 usage.
 	// The ToC<type> use the "C" locale.
 	// Typically, the "ToC" routines are only used in Element.
-	static bool ToLong(wxString const& inStr, long& outValue);
-	static long ToLong(wxString const& inStr);
-	static bool ToDouble(wxString const& inStr, double& outValue);
-	static double ToDouble(wxString const& inStr);
 	// bRetry is for wx2.9+: It fails on "123-23" now. Old behavior returned "123".
 	static bool ToCLong(wxString const& inStr, long& outValue, bool bRetry = false);
-	static int ToCLong(wxString const& inStr);
+	static int ToCLong(wxString const& inStr)
+	{
+		long val = 0;
+		ToCLong(inStr, val, true);
+		return static_cast<int>(val);
+	}
 	static bool ToCDouble(wxString const& inStr, double& outValue);
-	static double ToCDouble(wxString const& inStr);
+	static double ToCDouble(wxString const& inStr)
+	{
+		double val = 0.0;
+		ToCDouble(inStr, val);
+		return val;
+	}
 
 	static std::string Replace(
 			std::string const& inStr,
