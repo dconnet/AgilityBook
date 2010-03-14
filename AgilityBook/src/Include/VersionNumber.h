fd9beb81db8ab72c34c37359cbc68039c1612e26
David Connet <dcon@agilityrecordbook.com>
2010-03-14 19:52:57 +0000

    Started adding named settings in column assignment dialog

--- AgilityBook/src/Include/VersionNumber.h
+++ AgilityBook/src/Include/VersionNumber.h
@@ -43,7 +43,7 @@
 #define ARB_VER_MAJOR					2
 #define ARB_VER_MINOR					2
 #define ARB_VER_DOT						0
-#define ARB_VER_BUILD					2624
+#define ARB_VER_BUILD					2633
 
 #define ARB_VER_STR_MAJOR				STR(ARB_VER_MAJOR)
 #define ARB_VER_STR_MINOR				STR(ARB_VER_MINOR)
