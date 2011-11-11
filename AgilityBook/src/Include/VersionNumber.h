2a30cfd5a793fa12c6eb2bdd8020d53949fd41f5
David Connet <dcon@agilityrecordbook.com>
2011-11-11 23:49:50 +0000

    Change po compile script so I can use it in more things

--- AgilityBook/src/Include/VersionNumber.h
+++ AgilityBook/src/Include/VersionNumber.h
@@ -43,7 +43,7 @@
 #define ARB_VER_MAJOR					2
 #define ARB_VER_MINOR					3
 #define ARB_VER_DOT						2
-#define ARB_VER_BUILD					3225
+#define ARB_VER_BUILD					3240
 
 #define ARB_VER_STR_MAJOR				STR(ARB_VER_MAJOR)
 #define ARB_VER_STR_MINOR				STR(ARB_VER_MINOR)
--- AgilityBook/src/Win/res/compile.py
+++ AgilityBook/src/Win/res/compile.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python
 # Generate the message libraries and data files
 #
-# Requires gettext (msgcat/msgfmt)
+# Requires gettext (msgcat/msgfmt) in PATH
 #
 # Revision History
+# 2011-11-11 DRC Made generic for easy use in other projects.
 # 2009-03-05 DRC Fixed some parameter passing issues (spaces/hyphens in names)
 # 2009-03-01 DRC Support multiple po files (by using msgcat)
 # 2009-01-02 DRC Updated to support creation of data files
-"""compile.py [-d] executableDir targetname
+"""compile.py [-d] sourceDir firstFile executableDir targetname [lang;lang...]
 -d: Debugging mode (does not delete generated autogen.po file)
+sourceDir: Directory where .po files are
+firstFile: First .po file to process
 executableDir: Directory where executable is (where 'lang' dir is created)
 targetname: Name of .mo file to generate
+lang: Addition semi-colon separated languages (like 'fr_FR')
 """
 
 import getopt
@@ -21,7 +25,7 @@ import subprocess
 import zipfile
 
 # By default, gettext is in the path
-# This file is generated with msgcat of all po files (arb.po is first)
+# This file is generated with msgcat of all po files ('firstFile' is first)
 autogenFile = 'autogen.po'
 
 
@@ -49,7 +53,6 @@ def RunCommand(command, toastErr):
 
 def main():
 	bDebug = 0
-	executableDir = ''
 	try:
 		opts, args = getopt.getopt(sys.argv[1:], 'd')
 	except getopt.error, msg:
@@ -59,29 +62,42 @@ def main():
 	for o, a in opts:
 		if '-d' == o:
 			bDebug = 1
-	if 2 != len(args):
+	if len(args) < 4 or len(args) > 5:
 		print 'Usage:', __doc__
 		return 1
 
-	executableDir = args[0]
-	targetname = args[1]
+	langs = ['en_US']
+
+	if 5 == len(args):
+		langs += args[4].split(';')
+
+	sourceDir = args[0]
+	firstFile = args[1]
+	executableDir = args[2]
+	targetname = args[3]
+
+	if not os.access(sourceDir, os.F_OK):
+		print sourceDir, 'does not exist'
+		return 1
 
 	if not os.access(executableDir, os.F_OK):
 		print executableDir, 'does not exist'
+		return 1
 
 	langDir = os.path.join(executableDir, r'lang')
 	if not os.access(langDir, os.F_OK):
 		os.mkdir(langDir)
 
-	for file in ('en_US', 'fr_FR'):
-		autogen = os.path.join(file, autogenFile)
+	for langName in langs:
+		srcPath = os.path.join(sourceDir, langName)
+		autogen = os.path.join(langName, autogenFile)
 		# -t: output encoding
-		cmd = ['msgcat', '-t', 'utf-8', '-o', autogen, os.path.join(file, r'arb.po')]
-		for po in glob.glob(os.path.join(file, r'*.po')):
-			if po != os.path.join(file, r'arb.po') and po != autogen:
+		cmd = ['msgcat', '-t', 'utf-8', '-o', autogen, os.path.join(srcPath, firstFile)]
+		for po in glob.glob(os.path.join(srcPath, r'*.po')):
+			if po != os.path.join(srcPath, firstFile) and po != autogen:
 				cmd += [po]
 		RunCommand(cmd, 0)
-		installPath = os.path.join(langDir, file)
+		installPath = os.path.join(langDir, langName)
 		if not os.access(installPath, os.F_OK):
 			os.mkdir(installPath)
 		# -v: verbose
