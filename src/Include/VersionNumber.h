b25013b707ce7f2ab7e405ea7f9cd2c6eadf59b3
David Connet <dcon@agilityrecordbook.com>
2011-12-31 07:04:08 +0000

    Change copyright year

--- src/Include/VersionNumber.h
+++ src/Include/VersionNumber.h
@@ -55,5 +55,5 @@
 
 #define ARB_VERSION_CompanyName			"dcon Software"
 #define ARB_VERSION_FileDescription		"Agility Record Book"
-#define ARB_VERSION_LegalCopyright		"Copyright (c) 2002-2011 David Connet. All Rights Reserved."
+#define ARB_VERSION_LegalCopyright		"Copyright (c) 2002-2012 David Connet. All Rights Reserved."
 #define ARB_VERSION_ProductName			"Agility Record Book"
