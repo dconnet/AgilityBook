289b4a3a72c4036525f5f4de5b5f8b78e72da7a6
David Connet <dcon@agilityrecordbook.com>
2012-11-22 19:08:28 +0000

    Add Config40 test

--- AgilityBook/src/TestARB/TestConfig.cpp
+++ AgilityBook/src/TestARB/TestConfig.cpp
@@ -64,6 +64,7 @@ const wchar_t* const gc_Configs[] =
 	L"Config37_v13_1.xml",
 	L"Config38_v13_1.xml",
 	L"Config39_v13_2.xml",
+	L"Config40_v13_2.xml",
 };
 size_t gc_NumConfigs = sizeof(gc_Configs) / sizeof(gc_Configs[0]);
 
