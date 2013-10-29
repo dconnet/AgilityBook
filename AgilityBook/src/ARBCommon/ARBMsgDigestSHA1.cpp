db0c2da929797576b60dd4846b72798d9eb5d125
David Connet <dcon@agilityrecordbook.com>
2013-10-29 17:27:14 +0000

    Start adding sha1/sha256 hash functions

---
+++ AgilityBook/src/ARBCommon/ARBMsgDigestSHA1.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) David Connet. All Rights Reserved.
+ *
+ * License: See License.txt
+ */
+
+/**
+ * @file
+ * @brief File hashing algorithms
+ *
+ * Revision History
+ * @li 2013-10-29 DRC Created
+ */
+
+#include "stdafx.h"
+#include "ARBMsgDigestImpl.h"
+
+#include "ARBCommon/StringUtil.h"
+#include <sstream>
+#if defined(__WXWINDOWS__)
+#include <wx/file.h>
+#endif
+
+#if defined(__WXMSW__)
+#include <wx/msw/msvcrt.h>
+#endif
+
+// Note, error checking of arguments handled in ARBMsgDigest::Compute
+
+std::wstring ARBMsgDigestComputeSHA1(
+		wchar_t const* const inFileName,
+		size_t* outSize)
+{
+#pragma PRAGMA_TODO(implement sha1)
+	return std::wstring();
+}
+
+
+std::wstring ARBMsgDigestComputeSHA1(
+		std::istream& inFile,
+		size_t* outSize)
+{
+#pragma PRAGMA_TODO(implement sha1)
+	return std::wstring();
+}
