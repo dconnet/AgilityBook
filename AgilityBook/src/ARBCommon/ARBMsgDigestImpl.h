b3710fbc0adf2d061e09feeb1b87a8d4fae005e4
David Connet <dcon@agilityrecordbook.com>
2013-10-29 16:51:46 +0000

    Start adding sha1/sha256 hash functions

---
+++ AgilityBook/src/ARBCommon/ARBMsgDigestImpl.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) David Connet. All Rights Reserved.
+ *
+ * License: See License.txt
+ */
+
+/**
+ * @file
+ * @brief File hashing algorithms
+ *
+ * Revision History
+ * @li 2013-10-29 DRC Added more hash algorithms
+ */
+
+extern std::wstring ARBMsgDigestComputeMD5(
+		std::istream& inFile,
+		size_t* outSize);
+
+extern std::wstring ARBMsgDigestComputeSHA1(
+		std::istream& inFile,
+		size_t* outSize);
+
+extern std::wstring ARBMsgDigestComputeSHA256(
+		std::istream& inFile,
+		size_t* outSize);
--- AgilityBook/src/Include/ARBCommon/ARBMsgDigest.h
+++ AgilityBook/src/Include/ARBCommon/ARBMsgDigest.h
@@ -8,7 +8,7 @@
 
 /**
  * @file
- * @brief MD5 algorithm, based on RSA Data Security, Inc.
+ * @brief File hashing algorithms
  *
  * Revision History
  * @li 2012-04-10 DRC Based on wx-group thread, use std::string for internal use
@@ -23,7 +23,15 @@ class ARBMsgDigest
 {
 	ARBMsgDigest();
 public:
+	typedef enum
+	{
+		ARBDigestMD5,
+		ARBDigestSha1,
+		ARBDigestSha2,
+	} ARBDigest;
+
 	static std::wstring Compute(
 			std::istream& inFile,
-			size_t* outSize = NULL);
+			size_t* outSize = NULL,
+			ARBDigest type = ARBDigestMD5);
 };
