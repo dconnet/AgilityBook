cc9eb5d4eb3384d7155fa78d05c13db3c1b21fe7
David Connet <dcon@agilityrecordbook.com>
2014-05-16 21:27:50 +0000

    Rearrange HAS macros

--- src/Include/Platform/SetupARBPost.h
+++ src/Include/Platform/SetupARBPost.h
@@ -17,11 +17,16 @@
  * before include wx headers causes issues in msvc)
  *
  * Revision History
+ * 2014-05-16 Moved HAS macros here.
  * 2013-01-27 Split/moved stdafx.h.
  */
 
 // VC9/VC10 defines _HAS_TR1 if tr1 is present (via yvals.h).
 #include <memory>
+#if defined(__WXMAC__)
+#include <AvailabilityMacros.h>
+#endif
+
 // If not present, pick up boost's. Now we can use std::tr1::shared_ptr
 #if _MSC_VER < 1700 && (!defined(_HAS_TR1) || !_HAS_TR1)
 #if defined(__WXMAC__) && defined(MAC_OS_X_VERSION_10_9) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_9
@@ -78,4 +83,73 @@ namespace std
 #define ARRAY_SIZE(buf)	_countof(buf)
 #else
 #define ARRAY_SIZE(buf) (sizeof(buf) / sizeof(buf[0]))
+#endif
+
+
+// Compiler configuration
+
+// ARB_HAS_32_AND_64_BIT_TIMET
+// This define means the compiler has 2 time_ts
+// time_t: 32bits
+// __time64_t: 64bits
+#if _MSC_VER >= 1300 && _MSC_VER < 1400
+// VC6: time_t is 32bits
+// VC7: time_t is 32bits, and __time64_t is 64
+// VC8+: time_t is 64bit
+#define ARB_HAS_32_AND_64_BIT_TIMET
+#endif
+
+// ARB_HAS_SECURE_LOCALTIME
+//  _localtime64_s(struct tm*, time_t*)
+#if _MSC_VER >= 1400
+#define ARB_HAS_SECURE_LOCALTIME
+#endif
+
+// ARB_HAS_SECURE_MBS_WCS
+//  _wcstombs_s(size_t*, char*, size_t, const wchar_t*, size_t)
+#if _MSC_VER >= 1400
+#define ARB_HAS_SECURE_MBS_WCS
+#endif
+
+// ARB_HAS_SECURE_SPRINTF
+//  sprintf_s(char*, size_t, const char*, ...)
+#if _MSC_VER >= 1400
+#define ARB_HAS_SECURE_SPRINTF
+#endif
+
+// ARB_SET_ERASE_RETURNS_ITERATOR
+//  std::set<x>::iterator i = var.begin()...; i = var.erase(i); ...
+//  If not defined, will use "var.erase(i++);" construct
+#if defined(_MSC_VER) || (defined(MAC_OS_X_VERSION_10_9) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_9)
+#define ARB_SET_ERASE_RETURNS_ITERATOR
+#endif
+
+// ARB_HAS_GETSYSTEMINFO
+//  Has GetSystemInfo in order to check for x64.
+#if !defined(_MSC_VER) || _MSC_VER >= 1300
+#define ARB_HAS_GETSYSTEMINFO
+#endif
+
+// ARB_HAS_ISTREAM_WCHAR
+//  Supports ifstream(wchar_t*) (MS does, xcode 4.5 doesn't)
+#ifdef _MSC_VER
+#define ARB_HAS_ISTREAM_WCHAR
+#endif
+
+// ARB_HAS_OSTREAM_WCHAR
+//  Supports ofstream(wchar_t*) (MS does, xcode 4.5 doesn't)
+#ifdef _MSC_VER
+#define ARB_HAS_OSTREAM_WCHAR
+#endif
+
+// ARB_HAS_LAMBDA
+//  Supports lambda functions
+#if (defined(_MSC_VER) && _MSC_VER >= 1600) || (defined(MAC_OS_X_VERSION_10_9) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_9)
+#define ARB_HAS_LAMBDA
+#endif
+
+// nullptr support (see SetupARBPost.h for setting wx-based platforms)
+#if (defined(_MSC_VER) && _MSC_VER < 1600) || (defined(MAC_OS_X_VERSION_10_9) && MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_9)
+#else
+#define nullptr NULL
 #endif
