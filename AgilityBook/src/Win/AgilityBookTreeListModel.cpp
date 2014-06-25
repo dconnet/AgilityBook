fb2bd0bf1dd821aa02021260ff2c6c20cff84c74
David Connet <dcon@agilityrecordbook.com>
2014-06-25 02:02:44 +0000

    Changed Compare to CompareNoCase

--- AgilityBook/src/Win/AgilityBookTreeListModel.cpp
+++ AgilityBook/src/Win/AgilityBookTreeListModel.cpp
@@ -482,7 +482,7 @@ int CAgilityBookTreeListModel::Compare(
 	{
 		wxString str1 = value1.GetString();
 		wxString str2 = value2.GetString();
-		int res = StringUtil::CompareNoCase(str1, str2, true);
+		int res = StringUtil::CompareNoCase(str1, str2);
 		if (res)
 			return res;
 	}
