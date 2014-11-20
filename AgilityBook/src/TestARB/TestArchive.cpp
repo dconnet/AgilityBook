97c8183caa717e9c970bd59df51de1e04a06b19e
David Connet <dcon@agilityrecordbook.com>
2014-11-20 18:17:46 +0000

    Add archive test

--- AgilityBook/src/TestARB/TestArchive.cpp
+++ AgilityBook/src/TestARB/TestArchive.cpp
@@ -26,9 +26,31 @@
 
 SUITE(TestArchive)
 {
-#ifndef __WXWINDOWS__
-	static char const* FileData1 = "This is test1\nOk";
+	static char const* FileData1 = "This is test1\r\nOk";
 	static char const* FileData2 = "This is test2\r\nOk\r\n";
+
+#ifdef __WXWINDOWS__
+	TEST(ExtractFile)
+	{
+		//if (!g_bMicroTest)
+		{
+			CLibArchive archive(L"TestArchive.zip");
+
+			{
+				std::ostringstream data;
+				CHECK(archive.ExtractFile(L"test1.txt", data));
+				CHECK(data.str() == FileData1);
+			}
+
+			{
+				std::ostringstream data;
+				CHECK(archive.ExtractFile(L"test2.txt", data));
+				CHECK(data.str() == FileData2);
+			}
+		}
+	}
+
+#else
 	static char const* FileData3 = "This is test3\r\nReplaced\r\n";
 
 	static std::wstring CreateZip()
