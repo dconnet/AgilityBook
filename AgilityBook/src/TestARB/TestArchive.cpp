7bd7c50cc90a67a74b4f6b8ea1f0747fe5808da9
David Connet <dcon@agilityrecordbook.com>
2014-11-20 18:21:34 +0000

    Tweak archive test since it's Win only

--- AgilityBook/src/TestARB/TestArchive.cpp
+++ AgilityBook/src/TestARB/TestArchive.cpp
@@ -29,28 +29,35 @@ SUITE(TestArchive)
 	static char const* FileData1 = "This is test1\r\nOk";
 	static char const* FileData2 = "This is test2\r\nOk\r\n";
 
-#ifdef __WXWINDOWS__
-	TEST(ExtractFile)
+	TEST(ExtractFileFromRes)
 	{
-		//if (!g_bMicroTest)
+		if (!g_bMicroTest)
 		{
 			CLibArchive archive(L"TestArchive.zip");
 
 			{
 				std::ostringstream data;
+#ifdef WIN32
 				CHECK(archive.ExtractFile(L"test1.txt", data));
 				CHECK(data.str() == FileData1);
+#else
+				CHECK(!archive.ExtractFile(L"test1.txt", data));
+#endif
 			}
 
 			{
+#ifdef WIN32
 				std::ostringstream data;
 				CHECK(archive.ExtractFile(L"test2.txt", data));
 				CHECK(data.str() == FileData2);
+#else
+				CHECK(!archive.ExtractFile(L"test2.txt", data));
+#endif
 			}
 		}
 	}
 
-#else
+#ifndef __WXWINDOWS__
 	static char const* FileData3 = "This is test3\r\nReplaced\r\n";
 
 	static std::wstring CreateZip()
