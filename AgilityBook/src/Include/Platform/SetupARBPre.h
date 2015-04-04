e73aa74980a8fd65a2738733d3c6fa0c2afb1995
David Connet <dcon@agilityrecordbook.com>
2015-04-04 22:15:50 +0000

    add vc14 comment

--- AgilityBook/src/Include/Platform/SetupARBPre.h
+++ AgilityBook/src/Include/Platform/SetupARBPre.h
@@ -47,6 +47,7 @@
 // 1600: VC10.0 http://msdn.microsoft.com/en-us/library/b0084kay%28v=vs.100%29.aspx
 // 1700: VC11.0 http://msdn.microsoft.com/en-us/library/b0084kay%28v=vs.110%29.aspx
 // 1800: VC12.0 http://msdn.microsoft.com/en-us/library/b0084kay%28v=vs.120%29.aspx
+// 1900: VC14.0
 // _M_IX86: Defined for x86 (value specifies processor)
 // _M_X64: Defined for x64 processors
 // _M_IA64: Defined for Itanium processor family
