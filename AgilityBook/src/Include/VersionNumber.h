cd508d12ed86cd8d2844030b83ea968845d1241b
David Connet <dcon@agilityrecordbook.com>
2017-04-09 19:14:17 +0000

    v3.2.1

--- AgilityBook/src/Include/VersionNumber.h
+++ AgilityBook/src/Include/VersionNumber.h
@@ -37,7 +37,7 @@
 #define ARB_VER_MAJOR					3
 #define ARB_VER_MINOR					2
 #define ARB_VER_DOT						1
-#define ARB_VER_BUILD					99
+#define ARB_VER_BUILD					100
 
 #define ARB_VER_STR_MAJOR				STR(ARB_VER_MAJOR)
 #define ARB_VER_STR_MINOR				STR(ARB_VER_MINOR)
