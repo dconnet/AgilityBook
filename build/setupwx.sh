6398c8f9033a838e2adf0436b1f6d51c123db1c0
David Connet <dcon@agilityrecordbook.com>
2021-11-30 11:55:33 -0800

    Add SDK12, arm64 (arm64 is auto built when using sdk12)

--- build/setupwx.sh
+++ build/setupwx.sh
@@ -8,7 +8,7 @@
 # Note: Found old OSX SDKs at https://github.com/phracker/MacOSX-SDKs/releases
 #
 # History
-# 2021-11-30 Added SDK12.0
+# 2021-11-30 Added SDK12.0, arm64
 # 2021-04-14 Upgraded 3.1.4 to 3.1.5
 # 2021-01-20 Added SDK11.1
 # 2020-11-22 Added SDK11.0 (reordered to make older SDKs prefered)
@@ -142,6 +142,7 @@ Darwin*)
 	elif test -d /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.0.sdk; then
 		echo "Using 12.0 SDK"
 		TARGETSDK=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX12.0.sdk
+		TARGETARCH="--enable-macosx_arch=x86_64,arm64"
 		MIN_OS=$MAC_MIN_OS
 
 	fi
